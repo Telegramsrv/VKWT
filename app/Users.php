ea3f8564c8f7e4d76e3052bca113d115fe86fe9b
Oleksii Yaryi <oleksii.yaryi@gmail.com>
2017-01-10 13:53:30 +0200

    add new methods at model Users(qtyWallPost,qrtLikes,wallPosts and statistics)

--- app/Users.php
+++ app/Users.php
@@ -8,10 +8,33 @@ class Users extends Model
 {
 	protected $primaryKey = 'user_id';
 
-	private $user_id;
+	public function statistics()
+	{
+		$buffer = [];
+		$walls = $this->wallPosts();
+
+		foreach ($walls as $wall) {
+			$wall->date  = date('d-m-Y', $wall->date);
+			if (array_key_exists($wall->date,$buffer)){
+				$buffer[$wall->date] += $wall->likes;
+			}
+			else {
+				$buffer[$wall->date] = $wall->likes;
+			}
+		}
+		return $buffer;
+	}
+
+	public function qtyWallPosts(){
+		return $this->wallPosts()->count();
+	}
+
+	public function qtyLikes(){
+		return $this->wallPosts()->sum('likes');
+	}
 
 	public function wallPosts()
 	{
-		return Walls::where('user_id',$this->user_id)->get();
+		return $this->hasMany(Walls::class,'user_id');
 	}
 }
--- app/Walls.php
+++ app/Walls.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Walls extends Model
 {
-	//
+	protected $foreignKey = 'walls.user_id';
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
 }
