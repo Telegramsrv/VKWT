0174e7f0415517c94a6d81b78254e4112c95e5cd
TheArdent <gf9te34qwds1Nj>
2017-01-16 01:29:38 +0200

    add middleware with auth VK Api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Users;
 use Illuminate\Http\Request;
+use VK\VK;
 
 class UserController extends Controller
 {
-	public function getUser($id)
+	public function auth( Request $request)
+	{
+		$data = array(
+			'title' => 'VKWT | Главная страница',
+			'url'   => $request->cookie('url')
+		);
+		return view('pages.login',$data);
+	}
+
+	public function index(Request $request)
 	{
 
+	}
+
+	public function getUser($id)
+	{
+		dd(Users::find($id)->qtyLikes());
 	}
 }
